a2ba6ae82635b1d1d5749cae4da336f21fcd1b48
Mikael Brandt <mikael.brandt90@gmail.com>
2024-05-06 19:49:42 +0200

    some changes from work laptop i do not know

--- src/app/foobar/page.tsx
+++ src/app/foobar/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Here we are!
+      Here we are foobar!
     </main>
   )
 }
